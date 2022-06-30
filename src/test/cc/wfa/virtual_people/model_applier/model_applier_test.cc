2de2d2d69939858252afb8366a7bd7f144542b8a
wastadtlander <wastadt@google.com>
2022-06-30 22:41:07 +0000

    quick commit

--- src/test/cc/wfa/virtual_people/model_applier/model_applier_test.cc
+++ src/test/cc/wfa/virtual_people/model_applier/model_applier_test.cc
@@ -21,6 +21,7 @@
 #include "google/protobuf/text_format.h"
 #include "google/protobuf/io/zero_copy_stream_impl.h"
 #include "google/protobuf/util/message_differencer.h"
+#include "wfa/virtual_people/model_applier/model_applier.pb.h"
 
 using namespace ::google::protobuf;
 using namespace ::google::protobuf::io;
@@ -29,37 +30,37 @@ using namespace ::google::protobuf::util;
 namespace wfa_virtual_people {
 namespace {
 
-void ReadTextProtoFile(std::string path, Message* message) {
+void ReadTextProtoFile(std::string path, Message& message) {
     CHECK(!path.empty()) << "No path set";
     int fd = open(path.c_str(), O_RDONLY);
     CHECK(fd > 0) << "Unable to open file: " << path;
     FileInputStream fstream(fd);
-    CHECK(TextFormat::Parse(&fstream, message)) 
+    CHECK(TextFormat::Parse(&fstream, &message)) 
         << "Unable to parse textproto file: " << path;
 }
 
 TEST(ModelApplierTest, AllPossibleToyModelBranches) {
     std::string eventsExpectedPath = "src/test/cc/wfa/virtual_people/model_applier/textproto/expected/output_events.txt";
-    // std::string eventsOutputPath = "bazel-out/k8-fastbuild/bin/src/test/cc/wfa/virtual_people/model_applier/output_events.txt";
+    std::string eventsOutputPath = "bazel-out/k8-fastbuild/bin/src/test/cc/wfa/virtual_people/model_applier/output_events.txt";
 
-    // std::string reportsExpectedPath = "src/test/cc/wfa/virtual_people/model_applier/textproto/expected/output_reports.txt";
-    // std::string reportsOutputPath = "bazel-out/k8-fastbuild/bin/src/test/cc/wfa/virtual_people/model_applier/output_reports.txt";
+    std::string reportsExpectedPath = "src/test/cc/wfa/virtual_people/model_applier/textproto/expected/output_reports.txt";
+    std::string reportsOutputPath = "bazel-out/k8-fastbuild/bin/src/test/cc/wfa/virtual_people/model_applier/output_reports.txt";
     
-    Message* eventsExpected = nullptr;
+    LabelerOutputList eventsExpected;
     ReadTextProtoFile(eventsExpectedPath, eventsExpected);
 
-    // Message* eventsOutput = nullptr;
-    // ReadTextProtoFile(eventsOutputPath, eventsOutput);
+    LabelerOutputList eventsOutput;
+    ReadTextProtoFile(eventsOutputPath, eventsOutput);
 
-    // Message* reportsExpected = nullptr;
-    // ReadTextProtoFile(reportsExpectedPath, reportsExpected);
+    LabelerOutputList reportsExpected;
+    ReadTextProtoFile(reportsExpectedPath, reportsExpected);
 
-    // Message* reportsOutput = nullptr;
-    // ReadTextProtoFile(reportsOutputPath, reportsOutput);
+    LabelerOutputList reportsOutput;
+    ReadTextProtoFile(reportsOutputPath, reportsOutput);
 
-    // MessageDifferencer diff;
-    // EXPECT_TRUE(diff.Equals(*eventsExpected, *eventsOutput));
-    // EXPECT_TRUE(diff.Equals(*reportsExpected, *reportsOutput));
+    MessageDifferencer diff;
+    EXPECT_TRUE(diff.Equals(eventsExpected, eventsOutput));
+    EXPECT_TRUE(diff.Equals(reportsExpected, reportsOutput));
 }
 
 }   // namespace
